763b46e32cdf816236a01affd8b9f20d6e0a9f5c
xDanny <xdanndy@yahoo.com>
2019-03-12 14:00:30 -0700

    minor changes

--- Apocalypse Hollow Celeste/Assets/Scripts/CharacterMover.cs
+++ Apocalypse Hollow Celeste/Assets/Scripts/CharacterMover.cs
@@ -6,11 +6,58 @@ public class CharacterMover : MonoBehaviour
     public float moveSpeed = 10.0f;
     public bool facingRight = true;
 
-    public float jumpForce = 250.0f;
+    public float jumpForce = 10.0f;
     public Transform groundCheck;
     public float groundRadius = 0.2f;
     public bool grounded = false;
+    public bool doubleJump = false;
     public LayerMask whatIsGround;
+
+    public bool wallSlide;
+    public Transform wallCheckPoint;
+    public bool wallCheck;
+    public LayerMask wallLayerMask;
+
+    private void Update()
+    {
+        if (!grounded)
+        {
+            wallCheck = Physics2D.OverlapCircle(wallCheckPoint.position, 0.1f, wallLayerMask);
+            if (facingRight && Input.GetAxis("Horizontal") > 0.1f || !facingRight && Input.GetAxis("Horizontal") < 0.1f)
+            {
+                if (wallCheck)
+                {
+                    grounded = true;
+                    doubleJump = false;
+                    HandleWallSliding();
+                }
+            }
+        }
+        if (wallCheck == false || grounded)
+        {
+            wallSlide = false;
+        }
+    }
+
+    private void HandleWallSliding()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector3(GetComponent<Rigidbody2D>().velocity.x, -4f);
+
+        wallSlide = true;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (facingRight)
+            {
+                GetComponent<Rigidbody2D>().AddForce(new Vector3(-1, 4) * jumpForce);
+            }
+            else
+            {
+                GetComponent<Rigidbody2D>().AddForce(new Vector3(1, 4) * jumpForce);
+            }
+        }
+
+    }
 
     private void FixedUpdate()
     {
@@ -28,11 +75,21 @@ public class CharacterMover : MonoBehaviour
         // If touching ground
         grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
 
+        // Double jump
+        if (grounded)
+        {
+            doubleJump = false;
+        }
+
         // Character jump and checks if touching the ground
         bool jump = Input.GetButtonDown("Jump");
-        if (jump && grounded)
+        if (jump && (grounded || !doubleJump))
         {
             GetComponent<Rigidbody2D>().AddForce(Vector3.up * jumpForce);
+            if (!doubleJump && !grounded)
+            {
+                doubleJump = true;
+            }
         }
 
         // Flip Character by calling Flip() function, does this when right or left key is pressed
@@ -45,6 +102,22 @@ public class CharacterMover : MonoBehaviour
             Flip();
         }
 
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "MovingPlatform")
+        {
+            transform.parent = collision.transform;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "MovingPlatform")
+        {
+            transform.parent = null;
+        }
     }
 
     // Method to flip the character
