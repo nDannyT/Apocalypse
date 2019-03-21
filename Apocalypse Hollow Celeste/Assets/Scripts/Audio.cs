26f1234b53b59afd22db4f63da6236d258a0a778
xDanny <xdanndy@yahoo.com>
2019-03-21 00:00:03 -0700

    Tutorial + Level 1, spikes, respawn, Game Icon, and Portals

--- Apocalypse Hollow Celeste/Assets/Scripts/Audio.cs
+++ Apocalypse Hollow Celeste/Assets/Scripts/Audio.cs
@@ -6,7 +6,6 @@ public class Audio : MonoBehaviour
 {
     public AudioClip Music;
     public AudioSource MusicSource;
-
     // Start is called before the first frame update
     void Start()
     {
---
+++ Apocalypse Hollow Celeste/Assets/Scripts/Death.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Death : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform respawn;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        player.transform.position = respawn.transform.position;
+        //Debug.Log("Teleport!");
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
