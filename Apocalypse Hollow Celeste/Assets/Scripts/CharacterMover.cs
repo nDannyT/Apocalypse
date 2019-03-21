9cea120e7dc7b0168e53bc402b10db634209db27
xDanny <xdanndy@yahoo.com>
2019-03-21 12:16:53 -0700

    Level 3 with cannon, triggers, portal blockers, destroyed blocks, unclimbable walls, etc

--- Apocalypse Hollow Celeste/Assets/Scripts/CharacterMover.cs
+++ Apocalypse Hollow Celeste/Assets/Scripts/CharacterMover.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CharacterMover : MonoBehaviour
 {
@@ -17,6 +18,34 @@ public class CharacterMover : MonoBehaviour
     public Transform wallCheckPoint;
     public bool wallCheck;
     public LayerMask wallLayerMask;
+    public LayerMask slideLayerMask;
+
+    // Portals
+    public bool canTeleport = true;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "IgnorePortal")
+        {
+            Debug.Log("Nothing happens");
+            canTeleport = false;
+        }
+        else if (collision.gameObject.tag == "PortalTrigger")
+        {
+            Debug.Log("canTeleport = true");
+            canTeleport = true;
+        }
+        else if (collision.gameObject.tag == "Cannon")
+        {
+            Debug.Log("WEEEEEE");
+            GetComponent<Rigidbody2D>().AddForce(Vector3.up * jumpForce * 4, ForceMode2D.Impulse);
+        }
+
+        if (collision.gameObject.tag == "Portal" && canTeleport == true)
+        {
+            Debug.Log("Teleport!");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
 
     private void Update()
     {
@@ -34,8 +63,22 @@ public class CharacterMover : MonoBehaviour
                 }
             }
         }
-       // else
-        //{
+
+        // Check for unclimbable wall
+        if (!grounded)
+        {
+            wallCheck = Physics2D.OverlapCircle(wallCheckPoint.position, 0.1f, slideLayerMask);
+            if (facingRight && Input.GetAxis("Horizontal") > 0.1f || !facingRight && Input.GetAxis("Horizontal") < 0.1f)
+            {
+                if (wallCheck)
+                {
+                    grounded = true;
+                    doubleJump = true;
+                    HandleSliding();
+                }
+            }
+        }
+
         // Character jump and checks if touching the ground
         bool jump = Input.GetButtonDown("Jump");
         if (jump && (grounded || !doubleJump))
@@ -49,7 +92,6 @@ public class CharacterMover : MonoBehaviour
             }
             GetComponent<Rigidbody2D>().AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
         }
-        //}
         if (wallCheck == false || grounded)
         {
             wallSlide = false;
@@ -59,7 +101,7 @@ public class CharacterMover : MonoBehaviour
     // Wall sliding method
     private void HandleWallSliding()
     {
-        GetComponent<Rigidbody2D>().velocity = new Vector3(GetComponent<Rigidbody2D>().velocity.x, -4f);
+        GetComponent<Rigidbody2D>().velocity = new Vector3(GetComponent<Rigidbody2D>().velocity.x, -5f);
 
         wallSlide = true;
 
@@ -74,6 +116,15 @@ public class CharacterMover : MonoBehaviour
                 GetComponent<Rigidbody2D>().AddForce(new Vector3(1, 4) * jumpForce);
             }
         }
+
+    }
+
+    // Sliding from an unclimbable wall
+    private void HandleSliding()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector3(GetComponent<Rigidbody2D>().velocity.x, -8f);
+
+        wallSlide = true;
 
     }
 
---
+++ Apocalypse Hollow Celeste/Assets/Scripts/Trigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    public GameObject destroyThis;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Break!");
+        Object.Destroy(destroyThis);
+    }
+}
