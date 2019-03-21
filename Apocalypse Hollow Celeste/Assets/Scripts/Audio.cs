fa77af83829c42c7ef16f2f92a99204020508e36
xDanny <xdanndy@yahoo.com>
2019-03-20 21:33:10 -0700

    Added Audio and a Menu Screen

---
+++ Apocalypse Hollow Celeste/Assets/Scripts/Audio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+    public AudioClip Music;
+    public AudioSource MusicSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MusicSource.clip = Music;
+        MusicSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    MusicSource.Play();
+        //}
+    }
+}
